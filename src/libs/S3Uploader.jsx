0e91d43eb02e5c7545a0666c3939472ac7e01dc9
Roger Goldfinger <rgoldfinger@gmail.com>
2018-08-16 06:32:11 -0700

    uploader

--- src/libs/S3Uploader.jsx
+++ src/libs/S3Uploader.jsx
@@ -1,54 +1,85 @@
 // https://github.com/odysseyscience/react-s3-uploader
-var React = require('react');
-var createReactClass = require('create-react-class');
-var ReactDOM = require('react-dom');
-var PropTypes = require('prop-types');
-var ReactcreateClass = require('create-react-class');
-var S3Upload = require('./s3upload.js');
-var objectAssign = require('object-assign');
+import React from 'react';
+import S3Upload from './s3upload';
 
-var ReactS3Uploader = createReactClass({
-  displayName: 'ReactS3Uploader',
-
-  propTypes: {
-    onProgress: PropTypes.func,
-    onFinish: PropTypes.func,
-    onError: PropTypes.func,
-    size: PropTypes.number,
-  },
-
-  getDefaultProps: function() {
-    return {
-      onProgress: function(percent, message) {
-        console.log('Upload progress: ' + percent + '% ' + message);
-      },
-      onFinish: function(signResult) {
-        console.log('Upload finished: ' + signResult.publicUrl);
-      },
-      onError: function(message) {
-        console.log('Upload error: ' + message);
-      },
-    };
-  },
-
-  uploadFile: function(e) {
+class Uploader extends React.Component {
+  uploadFile = e => {
     e.stopPropagation();
     new S3Upload({
       size: this.props.size,
-      fileElement: this.getDOMNode(),
-      onProgress: this.props.onProgress,
-      onFinishS3Put: this.props.onFinish,
-      onError: this.props.onError,
+      fileElement: this.refs.fileInput,
+      onProgress:
+        this.props.onProgress ||
+        function(percent, message) {
+          console.log('Upload progress: ' + percent + '% ' + message);
+        },
+      onFinishS3Put:
+        this.props.onFinish ||
+        function(signResult) {
+          console.log('Upload finished: ' + signResult.publicUrl);
+        },
+      onError:
+        this.props.onError ||
+        function(message) {
+          console.log('Upload error: ' + message);
+        },
     });
-  },
+  };
 
-  render: function() {
-    return <input type="file" onChange={this.uploadFile} onClick={e => e.stopPropagation()} />;
-    // // return ReactDOM.input(objectAssign({}, this.props, {
-    // type: 'file',
-    // onChange: this.uploadFile,
-    // onClick: }
-  },
-});
+  render() {
+    return (
+      <input
+        type="file"
+        ref="fileInput"
+        onChange={this.uploadFile}
+        onClick={e => e.stopPropagation()}
+      />
+    );
+  }
+}
 
-export default ReactS3Uploader;
+// var ReactS3Uploader = createReactClass({
+//   displayName: 'ReactS3Uploader',
+//
+//   propTypes: {
+//     onProgress: PropTypes.func,
+//     onFinish: PropTypes.func,
+//     onError: PropTypes.func,
+//     size: PropTypes.number,
+//   },
+//
+//   getDefaultProps: function() {
+//     return {
+//       onProgress: function(percent, message) {
+//         console.log('Upload progress: ' + percent + '% ' + message);
+//       },
+//       onFinish: function(signResult) {
+//         console.log('Upload finished: ' + signResult.publicUrl);
+//       },
+//       onError: function(message) {
+//         console.log('Upload error: ' + message);
+//       },
+//     };
+//   },
+//
+//   uploadFile: function(e) {
+//     e.stopPropagation();
+//     new S3Upload({
+//       size: this.props.size,
+//       fileElement: this.getDOMNode(),
+//       onProgress: this.props.onProgress,
+//       onFinishS3Put: this.props.onFinish,
+//       onError: this.props.onError,
+//     });
+//   },
+//
+//   render: function() {
+//     return <input type="file" onChange={this.uploadFile} onClick={e => e.stopPropagation()} />;
+//     // // return ReactDOM.input(objectAssign({}, this.props, {
+//     // type: 'file',
+//     // onChange: this.uploadFile,
+//     // onClick: }
+//   },
+// });
+
+export default Uploader;
